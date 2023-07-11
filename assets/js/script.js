9b3754545ac9731a4b65eb97450a376a2050040a
Davide Belvisi <davide.belvisi@camelotbs.onmicrosoft.com>
2023-07-11 17:55:42 +0200

    new accordion script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+var current = null;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    // Collapse the currently expanded item
+    if (current !== null && current !== this) {
+      current.classList.remove("active");
+      var panel = current.nextElementSibling;
+      panel.style.maxHeight = null;
+    }
+
+    // Toggle the clicked item
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+    // Update the currently expanded item
+    current = (current === this) ? null : this;
+  });
+}
+
 // Element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
